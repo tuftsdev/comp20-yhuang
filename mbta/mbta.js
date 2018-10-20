ad8f7287d306923175a20e969f51cf2495316756
YiShu Huang <yishuhuang@YiShudeMacBook-Pro.local>
2018-10-20 14:07:43 -0400

    orders of the stations in the station array modified according to geographic locations

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -1,17 +1,14 @@
-function init()
-{			
-	//set up station list with objects
-	var station = [
+station = [
 	{
-		"name":"South Station",
-		"lat":"42.352271",
-		"lon":"-71.05524200000001",
-		"id":"place-sstat"
+		"name":"Alewife",
+		"lat":"42.395428",
+		"lon":"-71.142483",
+		"id":"place-alfcl"
 	},{
-		"name":"Andrew",
-		"lat":"42.330154",
-		"lon":"-71.057655",
-		"id":"place-andrw"
+		"name":"Davis",
+		"lat":"42.39674",
+		"lon":"-71.121815",
+		"id":"place-davis"
 	},{
 		"name":"Porter Square",
 		"lat":"42.3884",
@@ -23,45 +20,10 @@ function init()
 		"lon":"-71.118956",
 		"id":"place-harsq"
 	},{
-		"name":"JFK/UMass",
-		"lat":"42.320685",
-		"lon":"-71.052391",
-		"id":"place-jfk"
-	},{
-		"name":"Savin Hill",
-		"lat":"42.31129",
-		"lon":"-71.053331",
-		"id":"place-shmnl"
-	},{
-		"name":"Park Street",
-		"lat":"42.35639457",
-		"lon":"-71.0624242",
-		"id":"place-pktrm"
-	},{
-		"name":"Broadway",
-		"lat":"42.342622",
-		"lon":"-71.056967",
-		"id":"place-brdwy"
-	},{
-		"name":"North Quincy",
-		"lat":"42.275275",
-		"lon":"-71.029583",
-		"id":"place-nqncy"
-	},{
-		"name":"Shawmut",
-		"lat":"42.29312583",
-		"lon":"-71.06573796000001",
-		"id":"place-smmnl"
-	},{
-		"name":"Davis",
-		"lat":"42.39674",
-		"lon":"-71.121815",
-		"id":"place-davis"
-	},{
-		"name":"Alewife",
-		"lat":"42.395428",
-		"lon":"-71.142483",
-		"id":"place-alfcl"
+		"name":"Central Square",
+		"lat":"42.365486",
+		"lon":"-71.103802",
+		"id":"place-cntsq"
 	},{
 		"name":"Kendall/MIT",
 		"lat":"42.36249079",
@@ -73,10 +35,45 @@ function init()
 		"lon":"-71.070628",
 		"id":"place-chmnl"
 	},{
+		"name":"Park Street",
+		"lat":"42.35639457",
+		"lon":"-71.0624242",
+		"id":"place-pktrm"
+	},{
 		"name":"Downtown Crossing",
 		"lat":"42.355518",
 		"lon":"-71.060225",
 		"id":"place-dwnxg"
+	},{
+		"name":"South Station",
+		"lat":"42.352271",
+		"lon":"-71.05524200000001",
+		"id":"place-sstat"
+	},{
+		"name":"Broadway",
+		"lat":"42.342622",
+		"lon":"-71.056967",
+		"id":"place-brdwy"
+	},{
+		"name":"Andrew",
+		"lat":"42.330154",
+		"lon":"-71.057655",
+		"id":"place-andrw"
+	},{
+		"name":"JFK/UMass",
+		"lat":"42.320685",
+		"lon":"-71.052391",
+		"id":"place-jfk"
+	},{
+		"name":"North Quincy",
+		"lat":"42.275275",
+		"lon":"-71.029583",
+		"id":"place-nqncy"
+	},{
+		"name":"Wollaston",
+		"lat":"42.2665139",
+		"lon":"-71.0203369",
+		"id":"place-wlsta"
 	},{
 		"name":"Quincy Center",
 		"lat":"42.251809",
@@ -88,47 +85,49 @@ function init()
 		"lon":"-71.007153",
 		"id":"place-qamnl"
 	},{
-		"name":"Ashmont",
-		"lat":"42.284652",
-		"lon":"-71.06448899999999",
-		"id":"place-asmnl"
+		"name":"Braintree",
+		"lat":"42.2078543",
+		"lon":"-71.0011385",
+		"id":"place-brntn"
 	},{
-		"name":"Wollaston",
-		"lat":"42.2665139",
-		"lon":"-71.0203369",
-		"id":"place-wlsta"
+		"name":"Savin Hill",
+		"lat":"42.31129",
+		"lon":"-71.053331",
+		"id":"place-shmnl"
 	},{
 		"name":"Fields Corner",
 		"lat":"42.300093",
 		"lon":"-71.061667",
 		"id":"place-fldcr"
 	},{
-		"name":"Central Square",
-		"lat":"42.365486",
-		"lon":"-71.103802",
-		"id":"place-cntsq"
+		"name":"Shawmut",
+		"lat":"42.29312583",
+		"lon":"-71.06573796000001",
+		"id":"place-smmnl"
 	},{
-		"name":"Braintree",
-		"lat":"42.2078543",
-		"lon":"-71.0011385",
-		"id":"place-brntn"
+		"name":"Ashmont",
+		"lat":"42.284652",
+		"lon":"-71.06448899999999",
+		"id":"place-asmnl"
+	}
+];
 
-	}];
+function init()
+{		
+	// Set up the map	
 
-
-	var landmark = new google.maps.LatLng(42.352271, -71.05524200000001);
+	landmark = new google.maps.LatLng(42.352271, -71.05524200000001);
 			
-	// Set up map
-	var myOptions = {
+	myOptions = {
 		zoom: 13, 
 		center: landmark,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 
-	var themap = new google.maps.Map(document.getElementById("map"), myOptions);
+	themap = new google.maps.Map(document.getElementById("map"), myOptions);
 
 
-
+	//show all the stations on the map
 	displaystations(station, themap);
 
 }
